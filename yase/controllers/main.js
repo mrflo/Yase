f9b02f682bef50b45157b667d2600cbaea112580
Florent Grandjean <fgrandjean@sectorlight.com>
2015-10-28 16:25:41 +0000

    Object mapping

--- yase/controllers/main.js
+++ yase/controllers/main.js
@@ -6,14 +6,16 @@
 
 
 
-function MainCtrl($scope,GetArticle) {
+function MainCtrl($scope,GetArticle,$rootScope) {
 
     $scope.hello = "yo";
 
     var article = GetArticle;
-    
-    $scope.cards = article.get();
+    console.log(article);
+    $scope.cards = $rootScope.cards;
     console.log($scope.cards);
+    
+    
     /*$scope.cards = [
         { title : "Hello there!",
           body : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non tortor luctus, commodo turpis congue, faucibus felis. Proin tincidunt neque odio, in consectetur est sodales nec. In mollis vestibulum pulvinar. Sed iaculis luctus orci, tempor elementum dui luctus quis. Suspendisse euismod ornare nunc, eu laoreet orci. Vivamus facilisis, felis eget euismod laoreet, mauris lectus elementum sapien, at molestie nulla mauris non ante. Maecenas et urna a est viverra venenatis. Donec ullamcorper sodales eros id euismod. Donec vitae leo egestas, sodales nunc eget, congue magna.",
@@ -59,12 +61,14 @@ $scope.remove = function (index){
 }
 
 
-function GetArticle($http)
+function GetArticle($http, $rootScope)
 {
     var article = {};
+    
     //wikipedia call
     var url = "https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&rnlimit=5&grnlimit=5&exlimit=5&exintro&prop=extracts|images&format=json";
-        //"https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5&type=article&format=json";
+    //"https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5&type=article&format=json";
+    
     article.get = function(){
          $http({
              headers:{
@@ -79,7 +83,8 @@ function GetArticle($http)
         }).then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
-             return MapArticle(response);
+             $rootScope.cards =  MapArticle(response);
+             return;
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or serve r returns response with an error status.
@@ -95,14 +100,18 @@ function MapArticle(articles)
 {
     console.log(articles.data.query.pages);
     var cards = [];
-    for(var page in articles.data.query.pages)
+    var pages = articles.data.query.pages
+    for(var art in pages )
     {
+        var page = pages[art];
+        //console.log(page);
         var card = {};
-        //getOwnPropertyNames()
-        card['title']='test';
-        card['body']='ddsfs';
-        card['imgUrl']='';
+        card['title']=page.title;
+        card['body']=page.extract;
+        if(page.images)
+            card['imgUrl']=page.images[0].title;
         cards.push(card);
+ 
     }
     console.log(cards);
     return cards; //articles.data.query.pages;
